29d330a29979fcd68f1303aa69b918cd2060b422
aeez <makramfais@gmail.com>
2023-01-13 18:22:10 +0700

    card gallery

--- src/pages/Gallery.jsx
+++ src/pages/Gallery.jsx
@@ -1,14 +1,16 @@
 import React from "react";
+import Card from "../components/gallery/Card";
 import Navbar from "../components/Navbar";
+import Footer from "../components/Footer";
 
 export default function Gallery() {
   return (
     <>
-      <div className=" grid grid-cols-12 text-white shadow-xl">
-        <section className="navbar col-span-10 col-start-2">
+      <div className=" grid grid-cols-12">
+        <section className="col-span-10 col-start-2">
           <Navbar />
-
-          {/* Gallery */}
+          <Card />
+          <Footer />
         </section>
       </div>
     </>
