119239d72121178f467cf97e78bb8375e5cd5ec0
aeez <makramfais@gmail.com>
2023-01-17 09:54:32 +0700

    fix about

--- src/components/about/SectionAbout.jsx
+++ src/components/about/SectionAbout.jsx
@@ -1,3 +1,5 @@
+import { CheckCircleIcon } from "@heroicons/react/24/solid";
+
 const SectionAbout = () => {
   const Epoxy = [
     {
@@ -130,30 +132,48 @@ const SectionAbout = () => {
           dan harga yang relatif lebih murah dari kompetitor lainnya.
         </p>
       </div>
-      <p className="sm:text-md  mt-16 text-center  font-bold md:text-sm lg:text-2xl">
+      <p className="mt-16  text-center font-bold  sm:text-lg md:text-lg lg:text-2xl">
         BJA EPOXY dapat diaplikasikan untuk kebutuhan pada :
       </p>
-      <div className="px-20">
+      <div className="mt-5">
         {Epoxy.map((epoxy) => (
-          <div className="" key={epoxy.id}>
-            <p className="sm:text-md py-3 text-xs md:text-sm lg:text-base">
-              {epoxy.id}. {epoxy.name} : {epoxy.explained}
-            </p>
+          <div className="flex items-center pt-2 lg:px-20" key={epoxy.id}>
+            <div className="mr-3 text-green-800 sm:h-5 sm:w-5 md:h-7 md:w-7 lg:h-7 lg:w-7">
+              <CheckCircleIcon />
+            </div>
+            <div className="sm:text-md md:text-sm lg:text-base">
+              <p>
+                <span className="font-semibold">{epoxy.name}</span> :{" "}
+                {epoxy.explained}
+              </p>
+            </div>
           </div>
         ))}
       </div>
-      <p className="sm:text-md  mt-10 text-xs md:text-sm  lg:text-base"> </p>
-      Kami telah dipercayai oleh :
-      {diPercaya.map((dpr) => (
-        <div
-          className="my-6 flex items-center justify-center rounded-md bg-green-800 "
-          key={dpr.id}
-        >
-          <p className="text-center text-sm font-semibold text-white sm:text-lg md:text-lg">
-            {dpr.perusahaan} - {dpr.kota}
-          </p>
-        </div>
-      ))}
+      <p className="mt-14 text-center text-lg  font-bold ">
+        Kami telah dipercayai oleh:
+      </p>
+      <div className="mt-5 grid grid-cols-1 gap-y-6 gap-x-6 sm:grid-cols-1 sm:gap-y-8 md:grid-cols-3 lg:mt-10 lg:grid-cols-3 xl:grid-cols-3 xl:gap-x-8">
+        {diPercaya.map((dpr) => (
+          <div
+            className="box-mitra mx-10 flex h-24 items-center justify-center rounded-md bg-green-800 py-3 px-3 sm:mx-0 sm:h-28"
+            key={dpr.id}
+          >
+            <div className="">
+              <div className="mb-1 flex justify-center">
+                <p className="text-center text-sm font-semibold text-white sm:text-lg md:text-lg">
+                  {dpr.perusahaan}
+                </p>
+              </div>
+              <div className="flex justify-center">
+                <p className="sm:text-md text-center text-xs text-gray-100 md:text-sm lg:text-base">
+                  {dpr.kota}
+                </p>
+              </div>
+            </div>
+          </div>
+        ))}
+      </div>
     </div>
   );
 };
